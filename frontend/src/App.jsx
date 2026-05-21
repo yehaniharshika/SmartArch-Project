2d787b5bb9b8571024361fa448c1fa5aa196c211
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2026-05-21 12:00:23 +0530

    created uiComponents file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,25 @@
-import { useState } from "react";
-import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import DashboardPage from "./pages/DashboardPage";
+import UploadPage from "./pages/UploadPage";
+import ResultPage from "./pages/ResultPage";
+import ChatPage from "./pages/ChatPage";
 
-function App() {
+export default function App() {
   return (
-    <>
-      
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/"          element={<LandingPage />} />
+        <Route path="/login"     element={<LoginPage />} />
+        <Route path="/register"  element={<RegisterPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/upload"    element={<UploadPage />} />
+        <Route path="/result/:id" element={<ResultPage />} />
+        <Route path="/chat/:token" element={<ChatPage />} />
+        <Route path="*"          element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
